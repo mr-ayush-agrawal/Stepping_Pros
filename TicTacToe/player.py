a0fad4392410843e40dd3c059eb75f44c43f9113
Ayush <ayumhr2002@gmail.com>
2023-05-28 15:47:18 +0530

    Try expect for non int  val input while move

--- TicTacToe/player.py
+++ TicTacToe/player.py
@@ -10,15 +10,15 @@ class Player:
         print('\nIts {}\'s Move'.format(self.name))
         idx = None
         while(True):
-            idx=int(input("Enter the location Move "))
+
+            # Do soethinng for non int value
+            try:
+                idx=int(input("Enter the location Move "))
+            except:
+                print("Enter the valid Value in Numberical format")
+                continue
             
-            # print("The move is ", idx)
-            
-            # if not isinstance(idx, int):
-            #     print("Enter the valid Value in Numberical format")
-            #     continue
-            # # idx=int(idx)
-            
+            # print("The move is ", idx)            
             
             if not Board.is_safe(idx-1):
                 print("This Place is ALready filled")
@@ -30,9 +30,9 @@ class Player:
 
         
 ''' 
-# This is the Test of the file        
+This is the Test of the file        
 P=Player("name", 'X')
 frame=b.Board()
 P.move(frame)
 frame.show_Board()
-'''    
+'''
