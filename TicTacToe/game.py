265ee2b067e3e0e9e2ec06e1e79ddf0ee12df3e5
Ayush <ayumhr2002@gmail.com>
2023-05-29 02:37:31 +0530

    Integration of singlr player mode

--- TicTacToe/game.py
+++ TicTacToe/game.py
@@ -11,25 +11,55 @@ def getInfo():
     while (sym not in ['X','O']):
         sym = input("Enter the symbol of player 1 : ")[0].upper()
     players.append(Player(name,sym))
-    name = input("\nEnter the name of the player 2 : ")
+    if mode == 2 :
+        name = input("\nEnter the name of the player 2 : ")
+    else :
+        name = 'Computer'
+
+    if mode == 2 :
+        name = input("\nEnter the name of the player 2 : ")
+    else :
+        name = 'Computer'
+
     if sym == 'X':
         sym = 'O'
     else:
         sym = 'X'
+
+
     players.append(Player(name,sym))
+
+
 def printWinner(Winner):
     print("\n\nGAME OVER!!")
     print("The Winner is ",Winner.name)
 
+def getMode():
+    print("\nSelect the mode of the game ")
+    print("1. Single Player")
+    print("2. Multi Player")
+
+    while True:
+        try :
+            mode=int(input("\nEnter 1/2 Only : "))
+            if mode == 1 or mode == 2  :
+                return mode
+            else :
+                continue
+        except:
+            continue
+
+
 # Main Goes here
 
 display_Rule()
+mode=getMode()
 players = []
 getInfo()
 frame=brd.Board()
 
 for filled in range(9):
-    players[filled%2].move(frame)
+    players[filled%2].move(frame,mode)
     if frame.checkWin():
         printWinner(players[filled%2])
         break
