7f8630470c73134935fcd3831bdc82b0703b5870
Ayush <ayumhr2002@gmail.com>
2023-06-12 14:44:57 +0530

    Add Suffling

--- UNO/cards.cpp
+++ UNO/cards.cpp
@@ -1,24 +1,29 @@
 // This will be converted to the header file later Untill test its a cpp file
 
-# include <iostream>
+#include <iostream>
+#include <vector>
+#include <stdlib.h>
+#include <time.h>
+
 using namespace std;
 
-class cards 
+class cards
 {
-    string color;  // This is the suite of the card
-    char face;  // The Value of the card is teh face of the card
+    string color; // This is the suite of the card
+    char face;    // The Value of the card is teh face of the card
 
-    public :
-    cards(string clr,char num)
+public:
+    cards(string clr, char num)
     {
-        this->color=clr;
-        this->face=num;
+        this->color = clr;
+        this->face = num;
     }
 
     void show()
     {
         // This Function is temporary
-        cout<<"\n"<<color<<" -> "<<face;
+        cout << "\n"
+             << color << "\t-> " << face;
     }
 };
 /*
@@ -31,17 +36,39 @@ T -> Draw 2 (+2)
 F -> Draw 4 (+4)
 */
 
-void CreateDeck()
+vector<cards> CreateDeck()
 {
     // Creating all the cards present in the Deck
-    string clrs[] ={"Blue", "Green","Red", "Yellow"};
-    char nums[]={'1','2','3','4','5','6','7','8','9','10','T','S','R','W','F'};
-    int numsize=sizeof(nums),clrsize=4;
-    for(int i=0;i<numsize;i++)
+    string clrs[] = {"Blue", "Green", "Red", "Yellow"};
+    char nums[] = {'1', '2', '3', '4', '5', '6', '7', '8', '9', '0', 'T', 'S', 'R', 'W', 'F'};
+    int numsize = sizeof(nums), clrsize = 4;
+    vector<cards> deck;
+    for (int i = 0; i < numsize; i++)
     {
-        for(int j=0;j<clrsize;j++)
+        for (int j = 0; j < clrsize; j++)
         {
             // Creating thje card and Pusding it in a Vector
+            cards c(clrs[j], nums[i]);
+            deck.push_back(c);
         }
     }
+    return deck;
+}
+
+void suffleDeck(vector<cards> &deck) 
+{
+    srand(time(0));
+    for (int i=0,sz=deck.size(); i<sz;i++)
+    {
+        swap(deck[i],deck[rand()%sz]);
+    }
+}
+
+int main()
+{
+    vector<cards> deck = CreateDeck();
+    suffleDeck(deck);
+    for (cards c : deck)
+        c.show();
+    return 0;
 }
