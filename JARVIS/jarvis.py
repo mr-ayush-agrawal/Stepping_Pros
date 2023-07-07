48ae957e04184fe4c2b660b62570bc2024927c08
Ayush <me21b1076@iiitdm.ac.in>
2023-07-08 01:35:45 +0530

    Transfer basic functions to seprate file

--- JARVIS/jarvis.py
+++ JARVIS/jarvis.py
@@ -1,46 +1,47 @@
-import pyttsx3 as tts
-from datetime import datetime
-import speech_recognition as sr
-
+import wikipedia as wiki
+import webbrowser as web
+from audioStuff import *
 
 '''
 pyttsx3 -> Test to Speech
 datetime -> To get the current time
-
+speech_recognition -> To take the audio input
+wikipedia -> To search through wikipedia
+webbrowser -> open any link
 '''
 
-engine = tts.init('sapi5')
-voices = engine.getProperty('voices')
+def openSomething(query):
+    if 'brave' in query:
+        pass
+    elif 'youtube' in query :
+        speak("Opening Youtube in a new window", False)
+        web.open_new("youtube.com")
+    elif 'google' in query :
+        speak("Opening Google in a new window", False)
+        web.open_new("google.com")
 
-# To get the detail of the voices 
-# print(voices[1].id)
-
-# Setting up the Voices for the system
-engine.setProperty('voice',voices[0])
-
-
-
-def speak(audio):
-    '''This function takes an argument in string format and then Computer speaks that string
-    '''
-    engine.say(audio)
-    engine.runAndWait()
-
-def wishMe():
-    '''This Function will make your Jarvis wish you according to system time. And tell the system time also
-    '''
-    hour = datetime.now().hour
-    if hour >= 4 and hour < 12:
-        speak("Good Morning Sir !")
-    elif hour >= 12 and hour < 17 :
-        speak("Good AfterNoon Sir !")
-    else :
-        speak("Good Evening Sir !")
-    
-    curTime = datetime.now().strftime("%H:%M")
-    speak(f"Current time is {curTime}")
-    speak("I am Jarvis ! Please tell me how may I help you")
 
 if __name__ == '__main__':
-    # speak("Ayush is Awesome")
     wishMe()
+    # while True :
+    if True :
+        query = takeCommand().lower()
+
+        # Logic to implementing the tesks
+        if 'open' in query:
+            openSomething(query)
+
+        elif 'search' in query :
+            # searchSomething(query) -> To be defined same as openSomething ->Pending
+            pass
+        
+        elif 'wikipedia' in query:
+            query.replace('wikipedia' , "")
+            speak("Searching Wikipedia...\n")
+            result = wiki.summary(query, sentences = 2)
+            speak("According to wikipedia ...")
+            speak(result)
+
+        elif 'exit' in query :
+            speak("Exiting the program...", False)
+            exit()
