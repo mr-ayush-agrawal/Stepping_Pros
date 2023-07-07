b5970dfb46265d06fef300fb74c57af3c43881d5
Ayush <me21b1076@iiitdm.ac.in>
2023-07-08 02:54:29 +0530

    Basic assistant completed

--- JARVIS/jarvis.py
+++ JARVIS/jarvis.py
@@ -1,5 +1,6 @@
 import wikipedia as wiki
 import webbrowser as web
+import os
 from audioStuff import *
 
 '''
@@ -19,13 +20,30 @@ def openSomething(query):
     elif 'google' in query :
         speak("Opening Google in a new window", False)
         web.open_new("google.com")
+    elif 'stack overflow'  in query :
+        speak("Opening Stackoverflow in a new window", False)
+        web.open_new("stackoverflow.com")
+    elif 'mail' in query :
+        speak("Opening Mail Inbox in a new window", False)
+        web.open_new("https://mail.google.com/mail/u/0/#inbox")
+    elif 'email' in query :
+        speak("Opening Mail Inbox in a new window", False)
+        web.open_new("https://mail.google.com/mail/u/0/#inbox")
+
+    # Now opening Applications
+    elif 'code' in query :
+        codePath = r"C:\Users\Ayush\AppData\Local\Programs\Microsoft VS Code\Code.exe"
+        speak("Opening VS Code ", False)
+        os.startfile(codePath)
+
+
 
 if __name__ == '__main__':
     web.register('chrome', None)
 
-    # wishMe()
-    # while True :
-    if True :
+    wishMe()
+    while True :
+    # if True :
         query = takeCommand().lower()
 
         # Logic to implementing the tesks
@@ -37,12 +55,17 @@ if __name__ == '__main__':
             pass
         
         elif 'wikipedia' in query:
-            query.replace('wikipedia' , "")
-            speak("Searching Wikipedia...\n")
-            result = wiki.summary(query, sentences = 2)
-            speak("According to wikipedia ...")
-            speak(result)
+            try :
+                query.replace('wikipedia' , "")
+                speak("Searching Wikipedia...\n")
+                result = wiki.summary(query, sentences = 2)
+                speak("According to wikipedia ...")
+                speak(result)
+            except :
+                speak("Something went wrong... Please Try Again")
 
         elif 'exit' in query :
             speak("Exiting the program...", False)
             exit()
+
+        # Email Portion I am not making.    -> May be later in future
