114cb69992c7045a7c9fe34df8a6776812c4cb08
Ayush <me21b1076@iiitdm.ac.in>
2023-07-10 08:07:33 +0530

    Loading Sound Files

--- Flappy Bird/load.py
+++ Flappy Bird/load.py
@@ -17,3 +17,14 @@ def loadImages():
     GAME_IMAGES['pipe'] = (transform.rotate(image.load(PIPE).convert_alpha(), 180),
                         image.load(PIPE).convert_alpha()
                         )
+    GAME_IMAGES['bird'] = image.load('Gallery\\Images\\bird.png').convert_alpha()
+    GAME_IMAGES['background'] = image.load('Gallery\\Images\\Background.jpg').convert_alpha()
+    
+    
+
+def loadSounds():
+    GAME_SOUNDS['die'] = mixer.Sound("Gallery/Audio/die.mp3")
+    GAME_SOUNDS['hit'] = mixer.Sound("Gallery/Audio/hit.mp3")
+    GAME_SOUNDS['point'] = mixer.Sound("Gallery/Audio/point.wav")
+    GAME_SOUNDS['swoosh'] = mixer.Sound("Gallery/Audio/swoosh.wav")
+    GAME_SOUNDS['wing'] = mixer.Sound("Gallery/Audio/wing.wav")
