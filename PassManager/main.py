731083f0446cbb17b2a2e5f1e2702685e12ce47c
Ayush <me21b1076@iiitdm.ac.in>
2023-08-12 16:03:26 +0530

    Writing in file

---
+++ PassManager/main.py
@@ -0,0 +1,34 @@
+mstr = input ("Enter the master password :")
+menu = '''
+1. View Passwords
+2. Add Passord
+Enter Your Choice
+'''
+
+
+
+
+
+
+def view():
+    pass
+
+def Add():
+    Account = input('Enter the Account : ')
+    pswd = input('Enter the Password : ' )
+
+    with open("PassWords.txt", 'a') as file :
+        file.write(Account + " |$| " + pswd + '\n')
+        # |$| ->  is delimeter for seprating the Acc and pswd
+
+
+if __name__ == '__main__':
+    while True:
+        ch = input(menu)
+        if ch == '1' :
+            view()
+        elif ch == '2' :
+            Add()
+        else :
+            print("Exiting the Program")
+            break
