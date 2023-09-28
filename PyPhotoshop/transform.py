dc2c966cee19de72d83b2552506eb66fc1557566
Ayush <me21b1076@iiitdm.ac.in>
2023-09-29 03:55:53 +0530

    Blur Feature

--- PyPhotoshop/transform.py
+++ PyPhotoshop/transform.py
@@ -42,16 +42,48 @@ def adjustContast(image, factor, mid = 0.5):
     return newImage
 
 
+def blur(image, size):
+    '''size is the number of pixels to take into account whrn applying blur
+    The Pixels are from Left/Right/Top/Bottom/Diagonals
+    size should always be a odd number'''
+
+    # Creating new image
+    x_pixel, y_pixel, num_channels = image.array.shape
+    newImage = Image(x_pixel=x_pixel, y_pixel=y_pixel, num_channels=num_channels)
+
+    neighbour = size//2             # Number of neighbour at a side
+    for x in range(x_pixel):
+        for y in range(y_pixel):
+            for z in range(num_channels):
+                # Going through the neighbour and summing
+                total = 0
+                for x_i in range(max(0, x-neighbour), min(x+neighbour,x_pixel-1)+1):
+                    for y_i in range(max(0, y-neighbour), min(y_pixel-1,y+neighbour)):
+                        total += image.array[x_i,y_i,z]
+                    
+                # Normalizing it (As we are summing over squre)
+                newImage.array[x,y,z] = total/(size**2)
+
+    return newImage
+
+
 if __name__ == '__main__':
     if not getcwd().endswith('PyPhotoshop'):
         chdir(getcwd() + '/PyPhotoshop')
         # print(getcwd())
 
-    lake = Image(filename='lake.png')
+    # lake = Image(filename='lake.png')
     city = Image(filename='city.png')
 
-    # lets brighten the imgae
-    brightenImage = adjustContast(lake, .3)
-    brightenImage.write_image('lakeBright.png')
-    brightenImage = adjustContast(lake, 1.2)
-    brightenImage.write_image('lakeDark.png')
+    # lets Contrast the imgae
+    # brightenImage = adjustContast(lake, .3)
+    # brightenImage.write_image('lakeBright.png')
+    # brightenImage = adjustContast(lake, 1.2)
+    # brightenImage.write_image('lakeDark.png')
+
+    # Adding blur
+    blurImg = blur(city, 3)
+    blurImg.write_image('blurcity3.png')
+    blurImg = blur(city, 17)
+    blurImg.write_image('blurcity7.png')
+    
