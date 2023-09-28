48d7d3c834b8ceaf278bb45c2c8ad4b2f06e004a
Ayush <me21b1076@iiitdm.ac.in>
2023-09-29 04:28:04 +0530

    Edge detuction

--- PyPhotoshop/transform.py
+++ PyPhotoshop/transform.py
@@ -65,6 +65,42 @@ def blur(image, size):
                 newImage.array[x,y,z] = total/(size**2)
 
     return newImage
+    # '''Note : In this function we implementd the blur of kernal size n
+    # where each value is 1/n**2; for ex of size = 3 the kernal matrix is 
+    # [1/9 1/9 1/9]
+    # [1/9 1/9 1/9]
+    # [1/9 1/9 1/9]
+    # '''
+
+
+
+def apply_kernal(image, kernal):
+    '''Kernal should be a 2D matrix of any diamension
+    For simplycity we shall be using a NxN square matrix
+    '''
+
+    # Creating new image
+    x_pixel, y_pixel, num_channels = image.array.shape
+    newImage = Image(x_pixel=x_pixel, y_pixel=y_pixel, num_channels=num_channels)
+
+    neighbour = kernal.shape[0]//2
+    for x in range(x_pixel):
+        for y in range(y_pixel):
+            for z in range(num_channels):
+                # Going through the neighbour and summing
+                total = 0
+                for x_i in range(max(0, x-neighbour), min(x+neighbour,x_pixel-1)+1):
+                    for y_i in range(max(0, y-neighbour), min(y_pixel-1,y+neighbour)):
+                        x_k = x_i + neighbour - x
+                        y_k = y_i + neighbour - y
+                        kernal_val = kernal[x_k, y_k]
+                        total += image.array[x_i,y_i,z] * kernal_val
+
+                # Normalizing it (As we are summing over squre)
+                newImage.array[x,y,z] = total
+
+    return newImage
+
 
 
 if __name__ == '__main__':
@@ -72,7 +108,7 @@ if __name__ == '__main__':
         chdir(getcwd() + '/PyPhotoshop')
         # print(getcwd())
 
-    # lake = Image(filename='lake.png')
+    lake = Image(filename='lake.png')
     city = Image(filename='city.png')
 
     # lets Contrast the imgae
@@ -82,8 +118,24 @@ if __name__ == '__main__':
     # brightenImage.write_image('lakeDark.png')
 
     # Adding blur
-    blurImg = blur(city, 3)
-    blurImg.write_image('blurcity3.png')
-    blurImg = blur(city, 17)
-    blurImg.write_image('blurcity7.png')
+    # blurImg = blur(city, 3)
+    # blurImg.write_image('blurcity3.png')
+    # blurImg = blur(city, 17)
+    # blurImg.write_image('blurcity7.png')
     
+    # Edge detection using sobel's kernal
+    sobel_x_kernal = np.array([
+        [1, 2, 1],
+        [0, 0, 0],
+        [-1, -2, -1]
+    ])
+    sobel_y_kernal = np.array([
+        [1, 0, -1],
+        [2, 0, -2],
+        [1, 0, -1]
+    ])
+
+    edgeX = apply_kernal(city, sobel_x_kernal)
+    edgeX.write_image('Xedge.png')
+    edgeY = apply_kernal(city, sobel_y_kernal)
+    edgeY.write_image('Yedge.png')
