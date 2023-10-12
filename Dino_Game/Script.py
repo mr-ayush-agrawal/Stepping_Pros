8f0b4428ae6f00f3775e7ef1caf777e529816efd
Ayush <me21b1076@iiitdm.ac.in>
2023-10-12 14:56:14 +0530

    COlor effect and birds

--- Dino_Game/Script.py
+++ Dino_Game/Script.py
@@ -5,35 +5,29 @@ from time import sleep
 def hit(key):
     pg.keyDown(key)
 
-def isCollide(imdata):
-    for i in range (370,500):
+def isCollide(imdata,ref):
+    for i in range (370,510):
         for j in range(630, 740):
-            if imdata[i,j] >= 160:
+            if imdata[i,j] != ref:
                 return True
-    return False
+            
+    for i in range (370,710):
+        for j in range(630, 740):
+            if imdata[i,j] != ref:
+                return True
 
-    
-def draw():
-    '''This we are using for making the square for detecting the position of the obstacle'''
+    return False
 
 if __name__ == '__main__':
 
     sleep(2)
 
-    # while True :
-    #     # Taking SS of the screen in grey scale
-    #     image = ImageGrab.grab().convert('L')
-    #     # this is used to convert the image to arraafu frmat
-    #     imdata = image.load()
-
-    #     if isCollide(imdata):
-    #         hit('UP')
+    while True :
+        # Taking SS of the screen in grey scale
+        image = ImageGrab.grab().convert('L')
+        # this is used to convert the image to arraafu frmat
+        imdata = image.load()
+        ref = imdata[105,205]
+        if isCollide(imdata,ref):
+            hit('UP')
         
-
-    image = ImageGrab.grab()
-    imdata = image.load()
-    for i in range (370,500):
-        for j in range(630, 740):
-            imdata[i,j] = 160
-
-    image.show()
