13afe84bbe2260f3d822a28c276bf1af1a1a7a54
Ayush <me21b1076@iiitdm.ac.in>
2023-10-12 15:12:44 +0530

    DOne with this

--- Dino_Game/Script.py
+++ Dino_Game/Script.py
@@ -1,19 +1,18 @@
 import pyautogui as pg
-from PIL import Image, ImageGrab
+from PIL import ImageGrab
 from time import sleep
-
 def hit(key):
     pg.keyDown(key)
 
 def isCollide(imdata,ref):
-    for i in range (370,510):
+    for i in range (350,510):
         for j in range(630, 740):
             if imdata[i,j] != ref:
                 return True
             
-    for i in range (370,710):
-        for j in range(630, 740):
-            if imdata[i,j] != ref:
+    for i in range (710,800):
+        for j in range(500,630):
+            if abs(imdata[i,j] - ref) > 40 :
                 return True
 
     return False
